9c933c01df36f308ce06132a353f317733307c79
David Romaní <david@flux.cat>
2020-11-09 19:55:13 +0100

    improve frontend

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,13 @@
 import './styles/app.scss';
+import 'jquery.scrollto';
 
 const $ = require('jquery');
 require('bootstrap');
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
+    $('#sections-menu').bind('click', 'ul li a', function(event) {
+        event.preventDefault();
+        $.scrollTo(event.target.hash, 250, {offset: -90});
+    });
 });

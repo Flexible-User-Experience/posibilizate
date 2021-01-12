4636704a3dc23dfb82691e6d28a0a60496e2c1da
David Romaní <david@flux.cat>
2021-01-12 17:40:34 +0100

    add dynamic cookies & legal names

---
+++ src/Enum/CookieConsentNamesEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enum;
+
+class CookieConsentNamesEnum
+{
+    const COOKIE_CONSENT = '_PSBLZT_COOKIE_CONSENT';
+    const COOKIE_CONSENT_GTM = '_PSBLZT_COOKIE_CONSENT_GTM';
+}

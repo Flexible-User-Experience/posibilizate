6f6b1e7b0ce80aa696734d64ceb661ab20965e53
David Romaní <david@flux.cat>
2021-01-12 17:55:58 +0100

    add cookies page

--- src/Enum/CookieConsentNamesEnum.php
+++ src/Enum/CookieConsentNamesEnum.php
@@ -4,6 +4,6 @@ namespace App\Enum;
 
 class CookieConsentNamesEnum
 {
-    const COOKIE_CONSENT = '_PSBLZT_COOKIE_CONSENT';
-    const COOKIE_CONSENT_GTM = '_PSBLZT_COOKIE_CONSENT_GTM';
+    public const COOKIE_CONSENT = '_PSBLZT_COOKIE_CONSENT';
+    public const COOKIE_CONSENT_GTM = '_PSBLZT_COOKIE_CONSENT_GTM';
 }
